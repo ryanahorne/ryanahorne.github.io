454fb788cd80dbf0aef3520e980f13fb5c63b9af
ryanahorne <48040032+ryanahorne@users.noreply.github.com>
2020-03-06 20:52:04 -0500

    created Nav

--- scripts.js
+++ scripts.js
@@ -4,3 +4,12 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
       });
   });
+
+  function menuOpen() {
+    var x = document.getElementById("navLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
